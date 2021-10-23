44804af68bb1fde2cd47554c376249f27009c982
Rimaro03 <leonardo.ongaro2003@hotmail.com>
2021-10-23 12:14:25 +0200

    catalogue

--- js/catalogue-loader.js
+++ js/catalogue-loader.js
@@ -4,13 +4,20 @@ const catalogueDiv = document.querySelector('.catalogue')
 document.onreadystatechange = (e) => {
     if (document.readyState == 'complete') {
         const request = new XMLHttpRequest();
-        const url = "https://fakestoreapi.com/products?limit=9";
+        const url = "https://fakestoreapi.com/products";
         request.open('GET', url, true);
         request.send();
 
         request.onreadystatechange = (e) => {
             if (request.readyState === 4 && request.status === 200) {
-                let clothes = JSON.parse(request.responseText);
+                let clothesRES = JSON.parse(request.responseText);
+
+                let clothes = [];
+                for (let i = 0; i < 9; i++) {
+                    let index = Math.floor(Math.random() * clothesRES.length);
+                    clothes.push(clothesRES[index]);
+                    clothesRES.splice(index, 1);
+                }
 
                 fillPage(clothes);
             }
@@ -18,26 +25,27 @@ document.onreadystatechange = (e) => {
     }
 }
 
-
 //In abse ai filtri
-const typeChecks = document.querySelectorAll(".type")
-
 function checkBox() {
+    const typeChecks = document.querySelectorAll(".type")
+
+    catalogueDiv.innerHTML = ''
+
     let checks = [];
     typeChecks.forEach(checkBox => {
-        console.log(checkBox);
         if (checkBox.checked) {
             checks.push(checkBox.name.replace('-', ' '))
         }
     });
 
     if (checks.length == 0) {
+        console.log('nessun filtro');
         return;
     }
 
     checks.forEach(category => {
         const request = new XMLHttpRequest();
-        const url = `https://fakestoreapi.com/products/category/?limit=9`;
+        const url = `https://fakestoreapi.com/products/category/${category}`;
         request.open('GET', url, true);
         request.send();
 
@@ -49,49 +57,48 @@ function checkBox() {
             }
         }
     })
-
 }
 
 
 //Carica i prodotti
 function fillPage(clothes) {
-    let counter = 0;
-    for (let i = 0; i < 3; i++) {
-        let row = document.createElement('div')
-        row.classList.add('row')
-        for (let j = 0; j < 3; j++) {
-            let card = document.createElement('div')
-            card.classList.add('card')
-            let title = document.createElement('h4')
-            title.innerHTML = clothes[counter].title
-            card.append(title)
+    clothes.forEach(cloth => {
+        for (let i = 0; i < 3; i++) {
+            let row = document.createElement('div')
+            row.classList.add('row')
+            for (let j = 0; j < 3; j++) {
+                let card = document.createElement('div')
+                card.classList.add('card')
+                let title = document.createElement('h4')
+                console.log(cloth);
+                title.innerHTML = cloth.title
+                card.append(title)
 
-            let img = document.createElement('img');
-            img.src = clothes[counter].image;
+                let img = document.createElement('img');
+                img.src = cloth.image;
 
-            card.append(img)
+                card.append(img)
 
-            let list = document.createElement('ul')
-            list.classList.add('specs')
+                let list = document.createElement('ul')
+                list.classList.add('specs')
 
-            let rate = document.createElement('li')
-            rate.innerHTML = `Valutazione: ${clothes[counter].rating.rate}`
-            list.append(rate)
+                let rate = document.createElement('li')
+                rate.innerHTML = `Valutazione: ${cloth.rating.rate}`
+                list.append(rate)
 
-            let category = document.createElement('li')
-            category.innerHTML = `Categoria: ${clothes[counter].category}`
-            list.append(category)
+                let category = document.createElement('li')
+                category.innerHTML = `Categoria: ${cloth.category}`
+                list.append(category)
 
-            let price = document.createElement('li')
-            price.innerHTML = `Prezzo: ${clothes[counter].price}`
-            list.append(price)
+                let price = document.createElement('li')
+                price.innerHTML = `Prezzo: ${cloth.price}`
+                list.append(price)
 
-            card.append(list)
+                card.append(list)
 
-            row.append(card)
-
-            counter++;
+                row.append(card)
+            }
+            catalogueDiv.append(row)
         }
-        catalogueDiv.append(row)
-    }
+    });
 }
